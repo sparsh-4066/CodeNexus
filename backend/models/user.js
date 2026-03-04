7317246f8081850331b79a69c4ac96065881c049
sparsh-4066 <sparshuser1234@gmail.com>
2026-03-04 10:49:00 +0530

    This is the modified version of the earlier

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+{
   username: {
     type: String,
     required: true
@@ -16,7 +17,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
-
-}, { timestamps: true });
+},
+{ timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
